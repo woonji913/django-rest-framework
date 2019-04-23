27eb6210ffa66616a7f9bfa16dfbfa7a9ebe6a58
정운지 <jwji913@naver.com>
2019-04-23 06:35:27 +0000

    01_artist list && artist detail && comment create

--- api/settings.py
+++ api/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'musics.apps.MusicsConfig',
     'rest_framework',
+    'rest_framework_swagger',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- musics/views.py
+++ musics/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import MusicSerializer
-from .models import Music
+from .serializers import *
+from .models import *
 
 # Create your views here.
 
@@ -18,3 +18,28 @@ def music_list(request):
     musics = Music.objects.all()
     serializer = MusicSerializer(musics, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def music_detail(request, music_pk):
+    music = get_object_or_404(Music, pk=music_pk)
+    serializer =MusicSerializer(music)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def artist_list(request):
+    artists = Artist.objects.all()
+    serializer = ArtistSerializer(artists, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def artist_detail(request, artist_pk):
+    artist = get_object_or_404(Artist, pk=artist_pk)
+    serializer = ArtistDetailSerializer(artist)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def comment_create(request, music_pk):
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(music_id=music_pk)
+        return Response(serializer.data)
